7ab9001b8b2e13be0cd012b4fa7a24ea146ff7b2
nabetama <mao.nabeta@gmail.com>
2022-04-06 22:28:27 +0900

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,14 +75,14 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
+            serialize: ({query: {site, allMarkdownRemark}}) => {
               return allMarkdownRemark.nodes.map(node => {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + node.fields.slug,
                   guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
+                  custom_elements: [{"content:encoded": node.html}],
                 })
               })
             },
