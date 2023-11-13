ef1a627d9a24143f6e802aa4739e2fb6c5b93373
nabetama <mao.nabeta@gmail.com>
2023-11-13 10:55:08 +0900

    231110

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 630,
+              showCaptions: true,
             },
           },
           {
