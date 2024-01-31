0d761afe8158ed90faed384df294df85674ba3d3
nabetama <mao.nabeta@gmail.com>
2024-01-31 23:36:41 +0900

    .

--- scripts/write-post.ts
+++ scripts/write-post.ts
@@ -1,20 +1,20 @@
-const { writeFileSync } = require("fs");
-const { format } = require("date-fns");
+const { writeFileSync } = require('fs')
+const { format } = require('date-fns')
 
-console.log("Creating a new post...");
+console.log('Creating a new post...')
 // ./content/popsts の中に YYYY-MM-dd.mdx というファイル名でファイルを作成する
-const date = format(new Date(), "yyyy-MM-dd");
+const date = format(new Date(), 'yyyy-MM-dd')
 const content = `---
 title: ""
 date: "${date}"
 description: ""
-lastModDate: ""
+lastModDate:
 tags: []
 ---
 
-`;
+`
 
-const fileName = `${date}.mdx`;
-const filePath = `./content/posts/${fileName}`;
-writeFileSync(filePath, content);
-console.log(`Created a new post: ${filePath}`);
+const fileName = `${date}.mdx`
+const filePath = `./content/posts/${fileName}`
+writeFileSync(filePath, content)
+console.log(`Created a new post: ${filePath}`)
