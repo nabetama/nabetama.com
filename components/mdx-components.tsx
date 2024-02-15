1089f11443248e3f6807a2541f37c6634a72b9a5
nabetama <mao.nabeta@gmail.com>
2024-02-16 01:24:53 +0900

    design

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,8 +2,8 @@ import { useMDXComponent } from 'next-contentlayer/hooks'
 
 export function Image({ src, alt }: { src: string; alt: string }) {
   return (
-    <figure>
-      <img src={src} alt={alt} width={640} height={360} />
+    <figure className="my-8 mx-0">
+      <img className="mb-2" src={src} alt={alt} width={640} height={360} />
       <figcaption className="text-center text-xs">{alt}</figcaption>
     </figure>
   )
