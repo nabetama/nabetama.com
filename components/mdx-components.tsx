28e22874ade60dbeea48131bcdd1878bbf69836b
nabetama <mao.nabeta@gmail.com>
2024-02-16 01:15:08 +0900

    inject image component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,5 +1,13 @@
-import Image from 'next/image'
 import { useMDXComponent } from 'next-contentlayer/hooks'
+
+export function Image({ src, alt }: { src: string; alt: string }) {
+  return (
+    <figure>
+      <img src={src} alt={alt} width={640} height={360} />
+      <figcaption className="text-center text-xs">{alt}</figcaption>
+    </figure>
+  )
+}
 
 const components = {
   Image,
