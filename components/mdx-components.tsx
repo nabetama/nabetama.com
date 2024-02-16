aa94b0bdc77341f48ef7a710ed5333ed203f9de5
nabetama <mao.nabeta@gmail.com>
2024-02-16 20:33:05 +0900

    splited

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,13 +1,5 @@
 import { useMDXComponent } from 'next-contentlayer/hooks'
-
-export function Image({ src, alt }: { src: string; alt: string }) {
-  return (
-    <figure className="my-8 mx-0">
-      <img className="mb-2" src={src} alt={alt} width={640} height={360} />
-      <figcaption className="text-center text-xs">{alt}</figcaption>
-    </figure>
-  )
-}
+import { Image } from './image'
 
 const components = {
   Image,
